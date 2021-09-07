172717e0719f511639575d4fa148f818981465d4
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-09-06 21:27:01 -0700

    fixed bug

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -15,24 +15,24 @@ module.exports = [
       thirdparty: [
         // 可选，默认 []
         {
-          title: '在MDN中搜索',
+          title: 'Search by MDN',
           frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
           behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
         },
         {
-          title: '在Runoob中搜索',
+          title: 'Search by Runoob',
           frontUrl: 'https://www.runoob.com/?s=',
         },
         {
-          title: '在Vue API中搜索',
+          title: 'Search in Vue API',
           frontUrl: 'https://cn.vuejs.org/v2/api/#',
         },
         {
-          title: '在Bing中搜索',
+          title: 'Search by Bing',
           frontUrl: 'https://cn.bing.com/search?q=',
         },
         {
-          title: '通过百度搜索本站的',
+          title: 'Search by Baidu',
           frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
         },
       ],
@@ -49,20 +49,20 @@ module.exports = [
       showInMobile: false, // whether to display on the mobile side, default: false.
     },
   ],
-  [
-    'demo-block',
-    {
-      // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      settings: {
-        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-        // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-        jsfiddle: false, // 是否显示 jsfiddle 链接
-        codepen: true, // 是否显示 codepen 链接
-        horizontal: false, // 是否展示为横向样式
-      },
-    },
-  ],
+  // [
+  //   'demo-block',
+  //   {
+  //     // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+  //     settings: {
+  //       // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+  //       // cssLib: ['http://xxx'], // 在线示例中的css依赖
+  //       // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+  //       jsfiddle: false, // 是否显示 jsfiddle 链接
+  //       codepen: true, // 是否显示 codepen 链接
+  //       horizontal: false, // 是否展示为横向样式
+  //     },
+  //   },
+  // ],
   [
     'vuepress-plugin-zooming', // 放大图片
     {
