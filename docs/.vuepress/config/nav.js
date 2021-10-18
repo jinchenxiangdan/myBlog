260425fb932a1bcd549490a13bd7e5e722be7596
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-10-17 20:46:31 -0700

    disable navigator

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,64 +1,65 @@
 // nav
 module.exports = [
   // { text: '首页', link: '/' },
-  {
-    text: 'Notes',
-    link: '/notes/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-    items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      // {
-      //   text: 'Algorithm',
-      //   link: '/algorithm/',
-      //   items: [
-      //     { text: 'Sorting', link: '/algorithm/sorting/' },
-      //     { text: 'Searching', link: '/algorithm/searching/' },
-      //     { text: 'Data Structure', link: '/algorithm/data-structure/' }
-      //     // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-      //   ],
-      // },
-      {
-        text: 'Study Notes',
-        link: '/notes/',
-        items: [
-          { text: '《Statistic cookbook》', link: '/notes/javascript/' },
-          { text: '《Java高级程序设计》', link: '/notes/js/' },
-          { text: 'Git', link: '/notes/git/'}
+  // {
+  //   text: 'Notes',
+  //   link: '/notes/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  //   items: [
+  //     // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+  //     // {
+  //     //   text: 'Algorithm',
+  //     //   link: '/algorithm/',
+  //     //   items: [
+  //     //     { text: 'Sorting', link: '/algorithm/sorting/' },
+  //     //     { text: 'Searching', link: '/algorithm/searching/' },
+  //     //     { text: 'Data Structure', link: '/algorithm/data-structure/' }
+  //     //     // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+  //     //   ],
+  //     // },
+  //     {
+  //       text: 'Study Notes',
+  //       link: '/notes/',
+  //       items: [
+  //         // { text: '《Statistic cookbook》', link: '/notes/javascript/' },
+  //         // { text: '《Java高级程序设计》', link: '/notes/js/' },
+  //         { text: 'Git', link: '/notes/git/'}
 
-        ],
-      },
-    ],
-  },
-  {
-    text: 'Algorithm',
-    link: '/algorithm/',
-    items: [
-      { text: 'Sorting', link: '/algorithm/sorting/' },
-      { text: 'Searching', link: '/algorithm/searching/' },
-      { text: 'Data Structure', link: '/algorithm/data-structure/' }
-      // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-    ]
-  },
-  {
-    text: 'Data Science',
-    link: '/data-science/',
-    items: [
-      { text: 'Linear Algebra', link: '/data-science/linear-algebra/' },
-      { text: 'Statistic', link: '/data-science/statistic/' },
-      { 
-        text: 'Machine Learning', 
-        link: '/data-science/machine-learning/',
-        items: [
-          { text: 'Linear Algebra', link: '/data-science/machine-learning/linear-algebra/' },
-          { text: 'Linear Modelling', link: '/data-science/machine-learning/linear-modelling/' },
-          // { text: 'Bayesian Approach to ML', link: '/data-science/machine-learning/bayesian-approach-to-ml/' },
-          // { text: 'Classification', link: '/data-science/machine-learning/classification/' },
-          // { text: 'Clustering', link: '/data-science/machine-learning/clustering/' },
-          // { text: 'Nerual Networks', link: '/data-science/machine-learning/nerual-networks/' },
-        ]
-      },
-      {text: 'Data Mining', link: '/data-science/data-mining/' },
-    ],
-  },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: 'Algorithm',
+  //   link: '/algorithm/',
+  //   items: [
+  //     { text: 'Sorting', link: '/algorithm/sorting/' },
+  //     { text: 'Searching', link: '/algorithm/searching/' },
+  //     { text: 'Data Structure', link: '/algorithm/data-structure/' }
+  //     // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+  //   ]
+  // },
+  // {
+  //   text: 'Data Science',
+  //   link: '/data-science/',
+  //   items: [
+  //     { text: 'Linear Algebra', link: '/data-science/linear-algebra/' },
+  //     { text: 'Statistic', link: '/data-science/statistic/' },
+  //     { 
+  //       text: 'Machine Learning', 
+  //       link: '/data-science/machine-learning/',
+  //       items: [
+  //         { text: 'Linear Algebra', link: '/data-science/machine-learning/linear-algebra/' },
+  //         { text: 'Linear Modelling', link: '/data-science/machine-learning/linear-modelling/' },
+  //         // { text: 'Bayesian Approach to ML', link: '/data-science/machine-learning/bayesian-approach-to-ml/' },
+  //         // { text: 'Classification', link: '/data-science/machine-learning/classification/' },
+  //         // { text: 'Clustering', link: '/data-science/machine-learning/clustering/' },
+  //         // { text: 'Nerual Networks', link: '/data-science/machine-learning/nerual-networks/' },
+  //         // { text: 'Probability', link: '/data-science/machine-learning/nerual-networks/' },
+  //       ]
+  //     },
+  //     {text: 'Data Mining', link: '/data-science/data-mining/' },
+  //   ],
+  // },
   // {
   //   text: '更多',
   //   link: '/more/',
